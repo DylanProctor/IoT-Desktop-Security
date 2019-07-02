9e8aabd674bd242757d86a03917724e0560c2a9c
Dylan Ralph Proctor <DylanProctor@Dylans-MacBook.hsd1.ca.comcast.net>
2019-07-01 18:26:48 -0700

    update

---
+++ PIR_motion_detection.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import sys
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+outp = 23
+
+GPIO.setup(outp, GPIO.IN)
+
+
+try:
+	time.sleep(2)
+	while True:
+		if GPIO.input(outp):
+			print("Motion Detected")
+			time.sleep(5)
+		time.sleep(0.1)
+except:
+	GPIO.cleanup()
