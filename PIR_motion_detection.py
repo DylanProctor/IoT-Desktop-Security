df26f01d403a824613a294cf16ae7cada145f03e
Dylan Ralph Proctor <DylanProctor@Dylans-MacBook.local>
2019-07-16 10:55:04 -0700

    update

--- PIR_motion_detection.py
+++ PIR_motion_detection.py
@@ -2,18 +2,72 @@ import RPi.GPIO as GPIO
 import picamera
 import sys
 import time
+import pycurl, json
+
+import pubnub
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNOperationType, PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = "sub-c-d11e8f9a-9871-11e9-8994-3e832ec25d8b"
+pnconfig.publish_key = "pub-c-964c6e62-8094-417b-a4bd-979dd56a7d52"
+pnconfig.ssl = False
+pubnub = PubNub(pnconfig)
 
 GPIO.setmode(GPIO.BCM)
-
-outp = 23
-
-GPIO.setup(outp, GPIO.IN)
-
+#set the sensor to pin 23
+pir = 23
+GPIO.setup(pir, GPIO.IN)
+#set up the camera 
 camera = picamera.PiCamera()
+
+def MySubscribeCallback(SubscribeCallback):
+	def status(self, pubnub, status):
+		pass
+		
+		if status.operation == PNOperationType.PNSubscribeOperation \
+				or status.operation == PNOperationType.PNUnsubscribeOperation:
+			if status.category == PNStatusCategory.PNConnectedCategory:
+				pass
+
+			elif status.category == PNStatusCategory.PNReconnectedCategory:
+				pass
+
+			elif status.category == PNStatusCategory.PNDisconnectedCategory:
+				pass
+
+			elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+				pass
+			
+			elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+				pass
+
+			else:
+				pass
+		
+		elif status.operation == PNOperationType.PNSubscribeOperation:
+			if status.is_error():
+				pass
+			else:
+				pass
+		else:
+			pass
+
+	def presence(self, pubnub, presence):
+		pass
+
+	def message(self, pubnub, message):
+		pass
+		
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('ch1').execute()
 
 time.sleep(2)
 while True:
-	if GPIO.input(outp):
+	if GPIO.input(pir):
+		pubnub.publish().channel('ch1').message("Intruder Detected!").async(publish_callback)
 		print("Motion Detected")
 		time.sleep(3)
 	time.sleep(0.1)
