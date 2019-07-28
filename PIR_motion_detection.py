c692d1c854ec78f2d2b3302098241879d14ea25c
Dylan Ralph Proctor <DylanProctor@Dylans-MacBook.hsd1.ca.comcast.net>
2019-07-28 14:34:18 -0700

    updates

--- PIR_motion_detection.py
+++ PIR_motion_detection.py
@@ -20,56 +20,70 @@ pir = 23
 GPIO.setup(pir, GPIO.IN)
 flag = 0
 
-
 class MySubscribeCallback(SubscribeCallback):
-	def status(self, pubnub, status):
-		pass
-		
-		if status.operation == PNOperationType.PNSubscribeOperation \
-				or status.operation == PNOperationType.PNUnsubscribeOperation:
-			if status.category == PNStatusCategory.PNConnectedCategory:
-				pass
+    def status(self, pubnub, status):
+        pass
+        # The status object returned is always related to subscribe but could contain
+        # information about subscribe, heartbeat, or errors
+        # use the operationType to switch on different options
+        if status.operation == PNOperationType.PNSubscribeOperation \
+                or status.operation == PNOperationType.PNUnsubscribeOperation:
+            if status.category == PNStatusCategory.PNConnectedCategory:
+                pass
+                # This is expected for a subscribe, this means there is no error or issue whatsoever
+            elif status.category == PNStatusCategory.PNReconnectedCategory:
+                pass
+                # This usually occurs if subscribe temporarily fails but reconnects. This means
+                # there was an error but there is no longer any issue
+            elif status.category == PNStatusCategory.PNDisconnectedCategory:
+                pass
+                # This is the expected category for an unsubscribe. This means there
+                # was no error in unsubscribing from everything
+            elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle
+                # appropriately retry will be called automatically
+            elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+                pass
+                # This means that PAM does allow this client to subscribe to this
+                # channel and channel group configuration. This is another explicit error
+            else:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle appropriately
+                # retry will be called automatically
+        elif status.operation == PNOperationType.PNSubscribeOperation:
+            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
+            # For more information on how to configure heartbeat notifications through the status
+            # PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+            if status.is_error():
+                pass
+                # There was an error with the heartbeat operation, handle here
+            else:
+                pass
+                # Heartbeat operation was successful
+        else:
+            pass
+            # Encountered unknown status type
+ 
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+ 
+    # def message(self, pubnub, message):
+    #     if message.message == 'ON':
+    #     	global flag
+    #     	flag = 1
+    #     elif message.message == 'OFF':
+	# 		global flag
+	# 		flag = 0
 
-			elif status.category == PNStatusCategory.PNReconnectedCategory:
-				pass
-
-			elif status.category == PNStatusCategory.PNDisconnectedCategory:
-				pass
-
-			elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-				pass
-			
-			elif status.category == PNStatusCategory.PNAccessDeniedCategory:
-				pass
-
-			else:
-				pass
-		
-		elif status.operation == PNOperationType.PNSubscribeOperation:
-			if status.is_error():
-				pass
-			else:
-				pass
-		else:
-			pass
-
-	def presence(self, pubnub, presence):
-		pass
-
-	def message(self, pubnub, message):
-		# if message.message == 'ON':
-		# 	flag = 1
-		# elif message.message == 'OFF':
-		# 	flag = 0
-
-print("hello")
-
+ 
+ 
 pubnub.add_listener(MySubscribeCallback())
-pubnub.subscribe().channels('Ch2').execute()
+pubnub.subscribe().channels('ch1').execute()
 
 time.sleep(2)
 while True:
-	if GPIO.input(pir) #and flag == 1:
+	if GPIO.input(pir): #and flag == 1:
 		pubnub.publish().channel('Ch1').message("Intruder Detected!").async(publish_callback)
 		print("Motion Detected")
 		time.sleep(5)
