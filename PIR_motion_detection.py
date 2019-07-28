48ce20c4502f6d26bba06bf9c4f4d52cb83350d4
Dylan Ralph Proctor <DylanProctor@Dylans-MacBook.hsd1.ca.comcast.net>
2019-07-28 14:39:00 -0700

    updates

--- PIR_motion_detection.py
+++ PIR_motion_detection.py
@@ -84,7 +84,7 @@ pubnub.subscribe().channels('ch1').execute()
 time.sleep(2)
 while True:
 	if GPIO.input(pir): #and flag == 1:
-		pubnub.publish().channel('Ch1').message("Intruder Detected!").async(publish_callback)
+		pubnub.publish().channel('Ch1').message("Intruder Detected!").sync()
 		print("Motion Detected")
 		time.sleep(5)
 	time.sleep(0.1)
